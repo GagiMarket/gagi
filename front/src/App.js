2996be39718b6fcefa5da216b5c23c40e4498a76
Hanseunghoon <3246902@naver.com>
2021-01-11 15:25:05 +0900

    feat: 상품 등록, 리스트, 조회 페이지 개발 (#7)
    * Feat: 상품 등록 페이지 구현
    
    * create react app
    
    * feat: 상품 등록 페이지 구현
    
    * feat: 상품 리스트 페이지 구현
    
    * feat: 상품 조회 페이지 구현
    
    * update README

---
+++ front/src/App.js
@@ -0,0 +1,67 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import './reset.css';
+import './grid.min.css';
+
+import Post from './components/post';
+import List from './components/list';
+import Detail from './components/item-detail';
+
+class App extends React.Component {
+
+  // state = {
+  //   list-items: []
+  // }
+
+  // getPostListItem = async() =>{    
+  //   const {data : {data : {items}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+  //   this.setState({list-items:items});
+  // }
+
+  // async componentDidMount(){
+  //   this.getPostListItem();
+  // }
+
+  render()
+  {
+    // const {list-items} = this.state;
+
+    return (
+      <Fragment>
+        {/* 물품 등록 섹션 */}
+        <Post/>
+
+        {/* 등록된 물품 리스트 섹션 */}
+        <section className="item-list container">
+          <div className="row">
+
+              {/* {movies.map(movie => (
+                <List 
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))} */}
+              
+              <List/>
+              <List/>
+              <List/>
+              <List/>
+              <List/>
+              <List/>
+              <List/>
+              <List/>
+              <hr/> 
+              <Detail/>
+            </div>
+        </section>
+      </Fragment>
+    )
+  }
+}
+
+export default App;
