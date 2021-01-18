733bdb4005a8476e9cb20e7507695e6231d50f52
Hanseunghoon <3246902@naver.com>
2021-01-18 15:58:18 +0900

    feat: 페이지 이동 라우터 기능 구현 (#12)
    * feat: 페이지 헤더 구현
    
    * feat: 페이지 라우팅 기능 구현
    
    * refactor: react 구성 파일 수정

--- front/src/components/list.js
+++ front/src/components/list.js
@@ -1,27 +1,40 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import './list.css'
 
 function List({itemId, itemName, itemImage, itemPrice}){
   return (
-    <div className="col-12 col-md-3 list-item-div">
-      <a className="list-item" href="www.naver.com">
-        <div>
-          <img 
-            className="list-item-img" 
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTraEhh9z3iVmkBeMZAG7wnWVUr7ep9vE1Lmw&usqp=CAU" alt="아이폰 12"
-          />
-        </div>
+      <div className="col-12 col-md-3 list-item-div">
+        <Link to={{
+          pathname: `/itemlist/detail/${itemId}`,
+          state: {
+            itemId: itemId,
+            itemName: itemName,
+            itemImage: itemImage,
+            itemPrice: itemPrice
+          }
+        }}>
+          <a className="list-item" href="www.naver.com">
+            <div>
+              <img 
+                className="list-item-img" 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTraEhh9z3iVmkBeMZAG7wnWVUr7ep9vE1Lmw&usqp=CAU" alt="아이폰 12"
+              />
+            </div>
 
-        <div className="list-item-desc">
-          <div className="list-item-name">
-            맛있는 가지 1.5kg
-          </div>
-          <div className="list-item-price">
-            13000 원
-          </div>
-        </div>
-      </a>
-    </div>
+            <div className="list-item-desc">
+              <div className="list-item-name">
+                {itemName}
+                맛있는 가지 1.5kg
+              </div>
+              <div className="list-item-price">
+                {itemPrice}
+                13000 원
+              </div>
+            </div>
+          </a>
+        </Link>
+      </div>
   )
 }
 
