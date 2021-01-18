0b029694f0f83565a9f3cbc2757a11847d32f0a6
TaeWoong <audauddl2@gmail.com>
2021-01-18 15:58:06 +0900

    feat: Swagger 설정 (#10)

---
+++ back/src/main/java/com/gagi/market/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.gagi.market.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any()) // RequestMapping 에 할당된 모든 URL 목록 추출
+                .paths(PathSelectors.ant("/api/**")) // "/api/**" 인 URL 들만 필터링
+                .build();
+    }
+}
