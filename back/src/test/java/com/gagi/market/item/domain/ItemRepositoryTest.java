27dda084483f1c84509336ad1ba4cc0ff3f17fdd
doorisopen <audauddl2@gmail.com>
2021-01-18 19:49:26 +0900

    test: 페이징 조건을 넣어서 상품 목록을 조회한다

--- back/src/test/java/com/gagi/market/item/domain/ItemRepositoryTest.java
+++ back/src/test/java/com/gagi/market/item/domain/ItemRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -50,6 +52,31 @@ class ItemRepositoryTest {
 
         //then
         Assertions.assertThat(list.size()).isEqualTo(1);
+    }
+
+    @DisplayName("페이징 조건을 넣어서 상품 목록을 조회한다.")
+    @Test
+    public void findItemsByPageable() throws Exception {
+        //given
+        for (int i = 0; i < 20; i++) {
+            itemRepository.save(Item.builder()
+                    .itemName("m"+i+" 맥북 프로")
+                    .itemDescription("2021 신형 애플 노트북")
+                    .itemCategory("노트북")
+                    .itemPrice(10000+i)
+                    .itemLocation("강남역")
+                    .build());
+        }
+        //when
+        Page<Item> findItems = itemRepository.findAll(PageRequest.of(1, 5));
+
+        //then
+        Assertions.assertThat(findItems.getNumber()).isEqualTo(1);//현재 페이지 번호
+        Assertions.assertThat(findItems.getSize()).isEqualTo(5);//현재 조회된 데이터 개수
+        Assertions.assertThat(findItems.getTotalElements()).isEqualTo(20);//전체 데이터 개수
+        Assertions.assertThat(findItems.getTotalPages()).isEqualTo(4);//전체 페이지 개수
+        Assertions.assertThat(findItems.isFirst()).isFalse();//현재 첫 번째 페이지 인가?
+        Assertions.assertThat(findItems.hasNext()).isTrue();//다음 페이지 유무
     }
 
     @DisplayName("상품 하나를 삭제한다.")
