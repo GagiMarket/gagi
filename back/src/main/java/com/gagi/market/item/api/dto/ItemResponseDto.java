e0b2bebd51f857447e912e05ef37ed52001ef242
doorisopen <audauddl2@gmail.com>
2021-01-21 15:02:51 +0900

    feat: 상품 등록 및 수정 날짜 필드 추가

--- back/src/main/java/com/gagi/market/item/api/dto/ItemResponseDto.java
+++ back/src/main/java/com/gagi/market/item/api/dto/ItemResponseDto.java
@@ -3,6 +3,8 @@ package com.gagi.market.item.api.dto;
 import com.gagi.market.item.domain.Item;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ItemResponseDto {
     private Long itemId;
@@ -11,6 +13,8 @@ public class ItemResponseDto {
     private String itemCategory;
     private int itemPrice;
     private String itemLocation;
+    private LocalDateTime registerDate;
+    private LocalDateTime updateDate;
 
     public ItemResponseDto(Item item) {
         this.itemId = item.getItemId();
@@ -19,6 +23,8 @@ public class ItemResponseDto {
         this.itemCategory = item.getItemCategory();
         this.itemPrice = item.getItemPrice();
         this.itemLocation = item.getItemLocation();
+        this.registerDate = item.getRegisterDate();
+        this.updateDate = item.getUpdateDate();
     }
 
     public static ItemResponseDto of(Item item) {
