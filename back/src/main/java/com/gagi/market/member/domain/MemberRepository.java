a6cfd04a6d3f62d9de61967e8e3a3b3fcfdf4339
doorisopen <audauddl2@gmail.com>
2021-01-27 15:05:33 +0900

    refactor: 이메일과 비밀번호로 회원 정보를 조회한다

--- back/src/main/java/com/gagi/market/member/domain/MemberRepository.java
+++ back/src/main/java/com/gagi/market/member/domain/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean findMemberByMemberEmailAndMemberPw(String memberEmail, String memberPw);
+    Optional<Member> findMemberByMemberEmailAndMemberPw(String memberEmail, String memberPw);
     Optional<Member> findMemberByMemberEmail(String memberEmail);
     void deleteMemberByMemberEmail(String memberEmail);
 }
