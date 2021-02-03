dc24179cb65df1a47d0bb77c7e663fa8d9dffafd
doorisopen <audauddl2@gmail.com>
2021-02-03 21:36:42 +0900

    test: 회원과 상품 연관관계 API 테스트
    - 상품 등록, 수정, 삭제 시 로그인을 확인한다
    - 상품 수정, 삭제 시 상품에 대한 권한을 확인한다

--- back/src/test/java/com/gagi/market/item/api/ItemApiControllerTest.java
+++ back/src/test/java/com/gagi/market/item/api/ItemApiControllerTest.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gagi.market.item.api.dto.ItemRequestDto;
 import com.gagi.market.item.domain.Item;
 import com.gagi.market.item.domain.ItemRepository;
+import com.gagi.market.member.api.dto.SessionMember;
+import com.gagi.market.member.domain.Member;
+import com.gagi.market.member.domain.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,33 +36,49 @@ class ItemApiControllerTest {
     @LocalServerPort private int port;
     @Autowired private WebApplicationContext context;
     private MockMvc mvc;
+    protected MockHttpSession session;
 
     @Autowired
     private ItemRepository itemRepository;
-
+    @Autowired
+    private MemberRepository memberRepository;
 
     @BeforeEach
     public void setup() {
         mvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
+        Member member = memberRepository.save(Member.builder()
+                .memberEmail("test@gagi.com")
+                .memberPw("test")
+                .memberAddress("가지특별시 가지동")
+                .memberPhoneNumber("010-1234-5678")
+                .build());
+        session = new MockHttpSession();
+        session.setAttribute("SESSION_MEMBER", new SessionMember(member));
     }
 
     @AfterEach
     public void tearDown() {
         itemRepository.deleteAll();
+        session.clearAttributes();
     }
 
     @DisplayName("등록된 상품 목록을 조회한다.")
     @Test
     public void findItems() throws Exception {
         //given
-        itemRepository.save(Item.builder()
+        String memberEmail = "test@gagi.com";
+        Member findMember = memberRepository.findMemberByMemberEmail(memberEmail).get();
+        Item item = Item.builder()
                 .itemName("m1 맥북 프로")
                 .itemDescription("2021 신형 애플 노트북")
                 .itemCategory("노트북")
                 .itemPrice(10000)
                 .itemLocation("강남역")
-                .build());
+                .build();
+        item.createItem(findMember);
+        itemRepository.save(item);
+
         //when
         String url = LOCALHOST_URI + port + ITEM_API_URI;
         mvc.perform(
@@ -74,24 +94,31 @@ class ItemApiControllerTest {
     @Test
     public void findItemsByItemNameContains() throws Exception {
         //given
-        itemRepository.save(Item.builder()
+        String memberEmail = "test@gagi.com";
+        Member findMember = memberRepository.findMemberByMemberEmail(memberEmail).get();
+        Item item1 = Item.builder()
                 .itemName("m1 맥북 프로")
                 .itemDescription("2021 신형 애플 노트북")
                 .itemCategory("노트북")
                 .itemPrice(10000)
                 .itemLocation("강남역")
-                .build());
-        itemRepository.save(Item.builder()
+                .build();
+        item1.createItem(findMember);
+        itemRepository.save(item1);
+
+        Item item2 = Item.builder()
                 .itemName("에어팟 프로")
                 .itemDescription("2021 신형 애플 이어폰")
                 .itemCategory("이어폰")
                 .itemPrice(10000)
                 .itemLocation("강남역")
-                .build());
-        String searchField = "itemName";
-        String searchName = "프로";
+                .build();
+        item2.createItem(findMember);
+        itemRepository.save(item2);
 
         //when
+        String searchField = "itemName";
+        String searchName = "프로";
         String url = LOCALHOST_URI + port + ITEM_API_URI + "/search?" + searchField + "=" + searchName;
         mvc.perform(
                 get(url)
@@ -102,9 +129,9 @@ class ItemApiControllerTest {
         assertThat(findItems.size()).isEqualTo(2);
     }
 
-    @DisplayName("상품을 생성한다.")
+    @DisplayName("회원은 상품 등록을 성공한다.")
     @Test
-    public void createItem() throws Exception {
+    public void memberSuccessInCreateItem() throws Exception {
         //given
         String url = LOCALHOST_URI + port + ITEM_API_URI;
         ItemRequestDto requestDto = ItemRequestDto.builder()
@@ -118,6 +145,7 @@ class ItemApiControllerTest {
         mvc.perform(
                 post(url)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .session(session)
                         .content(new ObjectMapper().writeValueAsString(requestDto)))
                 .andExpect(status().isCreated());
         //then
@@ -127,17 +155,22 @@ class ItemApiControllerTest {
         assertThat(list.get(0).getItemCategory()).isEqualTo("노트북");
     }
 
-    @DisplayName("상품 정보를 수정한다.")
+    @DisplayName("권한을 가진 회원은 상품 정보 수정을 성공한다.")
     @Test
     public void updateItem() throws Exception {
         //given
-        Item item = itemRepository.save(Item.builder()
+        String memberEmail = "test@gagi.com";
+        Member findMember = memberRepository.findMemberByMemberEmail(memberEmail).get();
+        Item item = Item.builder()
                 .itemName("m1 맥북 프로")
                 .itemDescription("2021 신형 애플 노트북")
                 .itemCategory("노트북")
                 .itemPrice(10000)
                 .itemLocation("강남역")
-                .build());
+                .build();
+        item.createItem(findMember);
+        itemRepository.save(item);
+
         //when
         Long itemId = item.getItemId();
         String expectedItemName = "m1 맥북 에어";
@@ -152,31 +185,37 @@ class ItemApiControllerTest {
         mvc.perform(
                 put(url)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .session(session)
                         .content(new ObjectMapper().writeValueAsString(requestDto)))
-                .andExpect(status().is(201));
+                .andExpect(status().isCreated());
         //then
         List<Item> list = itemRepository.findAll();
         assertThat(list.size()).isEqualTo(1);
         assertThat(list.get(0).getItemName()).isEqualTo(expectedItemName);
     }
 
-    @DisplayName("상품 하나를 삭제한다.")
+    @DisplayName("권한을 가진 회원은 상품 삭제를 성공한다.")
     @Test
     public void deleteItem() throws Exception {
         //given
-        Item item = itemRepository.save(Item.builder()
+        String memberEmail = "test@gagi.com";
+        Member findMember = memberRepository.findMemberByMemberEmail(memberEmail).get();
+        Item item = Item.builder()
                 .itemName("m1 맥북 프로")
                 .itemDescription("2021 신형 애플 노트북")
                 .itemCategory("노트북")
                 .itemPrice(10000)
                 .itemLocation("강남역")
-                .build());
+                .build();
+        item.createItem(findMember);
+        itemRepository.save(item);
         //when
         Long itemId = item.getItemId();
         String url = LOCALHOST_URI + port + ITEM_API_URI + "/" + itemId;
         mvc.perform(
                 delete(url)
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .session(session))
                 .andExpect(status().isOk());
         //then
         List<Item> list = itemRepository.findAll();
