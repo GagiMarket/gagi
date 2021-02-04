bf39896d9cdf4550cab2c6e4672bad9aa343d688
doorisopen <audauddl2@gmail.com>
2021-02-04 19:16:52 +0900

    refactor: HTTP Method GET 예외 허용

--- back/src/main/java/com/gagi/market/config/auth/LoginCheckInterceptor.java
+++ back/src/main/java/com/gagi/market/config/auth/LoginCheckInterceptor.java
@@ -1,5 +1,6 @@
 package com.gagi.market.config.auth;
 
+import com.gagi.market.member.api.dto.SessionMember;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,19 +13,26 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
             "/api/v1.0/items/**",
             "/api/v1.0/members/**");
     public List<String> excludePathPatters = Arrays.asList(
-            "/api/v1.0/items",
             "/api/v1.0/items/search",
-            "/api/v1.0/members",
             "/api/v1.0/members/login",
             "/api/v1.0/members/duplicated");
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Object session_member = request.getSession().getAttribute("SESSION_MEMBER");
+        SessionMember session_member = (SessionMember) request.getSession().getAttribute("SESSION_MEMBER");
+        if (isGet(request)) {
+            return true;
+        }
         if (session_member == null) {
-            response.setStatus(407);
+            response.setStatus(401);
+            response.setHeader("message", "UnAuthorized!!");
             return false;
         }
         return true;
+    }
+
+    private boolean isGet(HttpServletRequest request) {
+        return (request.getRequestURI().equals("/api/v1.0/items")) && (request.getMethod().equals("GET")) ||
+                (request.getRequestURI().equals("/api/v1.0/members")) && (request.getMethod().equals("GET"));
     }
 }
